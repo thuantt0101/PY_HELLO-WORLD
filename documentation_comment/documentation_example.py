b3f879c3a1bb14c6ad22d29165110f2025f9ec35
thuantt0101 <thuantt01011992@gmail.com>
2023-04-08 15:17:39 +0700

    add  documentation comment

---
+++ documentation_comment/documentation_example.py
@@ -0,0 +1,59 @@
+
+
+def handle(self, myParam1, myParam2):
+    """
+    @param self: 
+    @param myParam1: 
+    @param myParam2: 
+    """
+    print("hello world!")
+
+handle()
+
+def handle1(self, myParam1, myParam2):
+    """
+    .. |language| replace:: Python
+    
+    This is an example of |language| function documentation
+
+    You can use text formating:
+    
+    * *italic*
+    * **bold**
+    * ``monospace``
+
+    Insert link:
+        `Pycharm <https://chat.zalo.me/>`
+    
+    provide code sample::
+
+        print('hello world!')
+    
+    Including those with command prompt:
+
+    >>> print('Hi!)
+    Hi!
+
+    Tables are also supported :
+
+    ==== ==== ======
+      Input Output
+    ------- --------
+      A  B  A or B
+    ==== ==== =====
+    false false false
+    true  true  true
+    ==== ==== =====
+
+    : param x: parameter description
+    : return: function return description
+        
+    """
+    print("hello world 1!")
+
+
+handle1()
+
+
+
+
